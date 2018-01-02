99d9ef578363cccf075c159f2d3a327b26b1aba9
gaofanni <gaofanni@4399inc.com>
2018-01-02 11:06:03 +0800

    增加proxy

--- config/index.js
+++ config/index.js
@@ -48,7 +48,15 @@ module.exports = {
         autoOpenBrowser: true,
         assetsSubDirectory: "static",
         assetsPublicPath: "/",
-        proxyTable: {},
+        proxyTable: {
+            '/redirect/': {
+                target: 'http://mobi.4399tech.com',
+                pathRewrite: {
+                    '^/redirect/': '/redirect/'
+                },
+                changeOrigin: true
+            }
+        },
         // CSS Sourcemaps off by default because relative paths are "buggy"
         // with this option, according to the CSS-Loader README
         // (https://github.com/webpack/css-loader#sourcemaps)
